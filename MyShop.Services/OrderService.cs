ecd5e9e708b01f86c3cc92e6721e6e3e19841c83
sejong055 <sejong055@gmail.com>
2020-05-27 15:50:03 +0900

    Arrangement

--- MyShop.Services/OrderService.cs
+++ MyShop.Services/OrderService.cs
@@ -33,5 +33,18 @@ namespace MyShop.Services
             orderContext.Insert(baseOrder);
             orderContext.Commit();
         }
+        public List<Order> GetOrders()
+        {
+            return orderContext.Collection().ToList();
+        }
+        public Order GetOrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+        public void UpdateOrder(Order updatedOrder)
+        {
+            orderContext.Update(updatedOrder);
+            orderContext.Commit();
+        }
     }
 }
